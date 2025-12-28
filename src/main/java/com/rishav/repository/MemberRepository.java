394c2db54e57b2595cf8573c78c0a108b000a61c
Rishav Raj Singh <Rishav Raj Singh@DESKTOP-09H1DO3>
2025-12-29 01:11:14 +0530

    feat: implement Book APIs using DTOs, pagination, and layered architecture

--- src/main/java/com/rishav/repository/MemberRepository.java
+++ src/main/java/com/rishav/repository/MemberRepository.java
@@ -1,5 +1,9 @@
 package com.rishav.repository;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.rishav.model.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Integer>{
+	
 }
